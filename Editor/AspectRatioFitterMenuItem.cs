c0128e5a8676c136a6e137883667c6d1292146f2
baba-s <shotababa1206@gmail.com>
2022-08-26 21:33:11 +0900

    add

---
+++ Editor/AspectRatioFitterMenuItem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kogane.Internal
+{
+    internal static class AspectRatioFitterMenuItem
+    {
+        [MenuItem( "CONTEXT/AspectRatioFitter/Set Default Aspect Ratio" )]
+        private static void SetDefaultAspectRatio( MenuCommand menuCommand )
+        {
+            var aspectRatioFitter  = ( AspectRatioFitter )menuCommand.context;
+            var defaultAspectRatio = GetDefaultAspectRatio( aspectRatioFitter );
+
+            Undo.RecordObject( aspectRatioFitter, "Set Default Aspect Ratio" );
+
+            aspectRatioFitter.aspectRatio = defaultAspectRatio;
+        }
+
+        private static float GetDefaultAspectRatio( Component component )
+        {
+            if ( component.TryGetComponent<Image>( out var image ) )
+            {
+                var sprite = image.sprite;
+                var rect   = sprite.rect;
+
+                return rect.width / rect.height;
+            }
+
+            if ( component.TryGetComponent<RawImage>( out var rawImage ) )
+            {
+                var texture = rawImage.texture;
+
+                return ( float )texture.width / texture.height;
+            }
+
+            return 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.AspectRatioFitterMenuItem
+# Kogane Aspect Ratio Fitter Menu Item
+
+AspectRatioFitter の aspectRatio にスプライトのアスペクト比を設定できるようにするエディタ拡張
